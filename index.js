4d90ad3dc5132d484baf01747a29466e302348e1
Jeremy Smith <78971664+backbeatmedia@users.noreply.github.com>
2022-11-24 10:17:37 +0000

    Publish

---
+++ index.js
@@ -0,0 +1,45 @@
+const sanityClient = require('@sanity/client')
+const path= require('path')
+const fs = require('fs')
+
+
+
+module.exports = async (
+  job,
+  settings,
+  {
+    input,
+    params,
+    document, 
+    fieldName, 
+    fieldType,
+    filename
+  },
+  type
+) => {
+
+  const client = sanityClient(params)
+
+  var filePath = input || job.output
+
+  if (!path.isAbsolute(filePath)) filePath = path.join(job.workpath, filePath)
+
+  const asset = await client.assets.upload(fieldType,
+                    fs.createReadStream(filePath),
+                      {filename: filename
+                        })
+  
+  await client.patch(document)
+                  .set({
+                    [fieldName]: {
+                      _type: fieldType,
+                      asset: {
+                        _type: "reference",
+                        _ref: asset._id
+                      }
+                    }
+                  })
+                  .commit()
+
+}
+
