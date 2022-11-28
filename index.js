dc140cb949867bfc1a2fe3d3500d7ee6ce41984e
Jeremy Smith <78971664+backbeatmedia@users.noreply.github.com>
2022-11-28 11:05:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const sanityClient = require('@sanity/client')
-const path= require('path')
+const path = require('path')
 const fs = require('fs')
-
-
 
 module.exports = async (
   job,
@@ -10,36 +8,51 @@ module.exports = async (
   {
     input,
     params,
-    document, 
-    fieldName, 
+    document,
+    fieldName,
     fieldType,
     filename
   },
   type
 ) => {
 
+  // make a Sanity client
   const client = sanityClient(params)
 
+  // find where the asset to be uploaded is stored
   var filePath = input || job.output
-
   if (!path.isAbsolute(filePath)) filePath = path.join(job.workpath, filePath)
 
-  const asset = await client.assets.upload(fieldType,
-                    fs.createReadStream(filePath),
-                      {filename: filename
-                        })
-  
-                await client.patch(document)
-                  .set({
-                    [fieldName]: {
-                      _type: fieldType,
-                      asset: {
-                        _type: "reference",
-                        _ref: asset._id
-                      }
-                    }
-                  })
-                  .commit()
+  // discover any existing asset stored in the field
+  const fields = `{${fieldName}{asset->}}`;
+  const exisiting = await client.fetch(`*[_id == \"${document}\"] ${fields}`, {});
+
+  // upload the new asset
+  const uploaded = await client.assets.upload(fieldType,
+    fs.createReadStream(filePath),
+    {
+      filename: filename
+    })
+
+  // patch the new asset into the field
+  await client.patch(document)
+    .set({
+      [fieldName]: {
+        _type: fieldType,
+        asset: {
+          _type: "reference",
+          _ref: uploaded._id
+        }
+      }
+    })
+    .commit()
+
+  // if there was already an asset in that field - delete it
+  if (existing[fieldName]) {
+
+    await client.delete(existing[fieldName]._id)
+
+  }
 
 }
 
